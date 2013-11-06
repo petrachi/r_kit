5bbe56fb51ce6587972491488e469a27085015ae
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2013-11-06 22:26:37 +0100

    add decorable module + helper

--- lib/r_kit.rb
+++ lib/r_kit.rb
@@ -1,6 +1,8 @@
 require "r_kit/version"
 require "r_kit/engine"
 
+require "r_kit/decorable"
+
 module RKit
   # Your code goes here...
 end
---
+++ lib/r_kit/decorable.rb
@@ -0,0 +1,26 @@
+module Blog
+  module Decorable
+    def decorate(view_context)
+      @view_context = view_context
+    
+      class << self 
+        include DecoratorMethods
+      end
+    
+      self
+    end
+  
+    def h
+      @view_context
+    end
+  end
+  
+  module DecorableHelper
+    def decorate obj, &block
+      capture do
+        block.call obj.decorate(self)
+      end
+    end
+  end
+  
+end
