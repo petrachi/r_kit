1b6629cafb2f9fcc2645db7e7bead98393991d2e
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2014-07-02 22:44:43 +0200

    improve backtrace

--- lib/r_kit/backtrace/kernel_extend.rb
+++ lib/r_kit/backtrace/kernel_extend.rb
@@ -1,8 +1,24 @@
-require 'continuation'
-
 module Kernel
 
-  def backtrace &block
+  def _backtrace_from_ruby_vm &block
+    stack_level = 0
+    block ||= ->(obj){ obj != self }
+
+    RubyVM::DebugInspector.open do |inspector|
+      loop do
+        stack_binding = begin
+          inspector.frame_binding(stack_level += 1)
+        rescue ArgumentError
+          nil
+        end
+
+        obj = stack_binding.try :eval, 'self'
+        return obj if obj != self && block.call(obj)
+      end
+    end
+  end
+
+  def _backtrace_from_continuation &block
     cc = nil
     block ||= ->(obj){ obj != self }
 
@@ -17,4 +33,11 @@ module Kernel
     backtrace unless backtrace.is_a? Continuation
   end
 
+
+  if RubyVM.const_defined? "DebugInspector"
+    alias :backtrace :_backtrace_from_ruby_vm
+  else
+    require 'continuation'
+    alias :backtrace :_backtrace_from_continuation
+  end
 end
