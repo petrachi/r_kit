4ce70e89209d81a09f618249918f998be03fa64f
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2014-10-05 14:53:19 +0200

    improve utility

--- lib/r_kit/utility.rb
+++ lib/r_kit/utility.rb
@@ -16,4 +16,11 @@ class RKit::Utility
   # TODO: module_extend: to_class
   # TODO: enumerable_extend/or-array_extend: with_indifferent_access
   # TODO: get back some code from hash_extend && array_extend gems
+
+
+  # TODO: Today, the files are structured by class they extend, not by feature
+  # TODO: we first encounter a feature multi class with the 'then' menthod (kernel, nil, false)
+  # -> not true anymore for 'then', it's only in kernel now
+  # TODO, maybe, we should create a module/feature, then include the modules into corresponding classes
+  # TODO: and, this has the advantage to be more readable on a RKit::CONFIG point of view (POV)
 end
--- lib/r_kit/utility/kernel_extend.rb
+++ lib/r_kit/utility/kernel_extend.rb
@@ -16,4 +16,30 @@ module Kernel
   def running_script? script
     Regexp.new(script) =~ running_script
   end
+
+
+  # TODO: think about a 'else' method => x.then{}.else{}
+  # it could be done, when the 'then' return 'nil', we define an instance var in the singleton (possible on nil ?)
+  # then reuse that into the else, as an arg to the block
+  def then **options, &block
+    if self && conditionnal_statement(options)
+      block.call(self)
+    else
+      self
+    end
+  end
+
+  def conditionnal_statement **options
+    options.slice(:if, :unless).reduce(true) do |continue, (statement, condition)|
+      continue && send(condition).tap do |closure|
+        case statement
+        when :if
+          !!closure
+        when :unless
+          !closure
+        end
+      end
+    end
+  end
+
 end
