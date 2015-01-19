9e9aee38739caa2ae6ace482bd26a042813932b8
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2015-01-19 21:23:21 +0100

    improve grid

--- lib/r_kit/grid/sass_extend.rb
+++ lib/r_kit/grid/sass_extend.rb
@@ -1,13 +1,13 @@
 module Sass::Script::Functions
 
-  base_width = if RKit::Grid.config.full_width
+  grid_base_width = if RKit::Grid.config.full_width
     value, unit = RKit::Grid.config.full_width
     Sass::Script::Number.new value / 94, unit
   else
     Sass::Script::Number.new *RKit::Grid.config.base_width
   end
 
-  define_method 'base_width' do
-    base_width
+  define_method 'grid_base_width' do
+    grid_base_width
   end
 end
